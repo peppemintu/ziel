6795224e7d59fdf089b8ee9dcd2fbe232aa51e54
akot <anastasiya.kot@epolsoft.com>
2025-03-17 19:37:58 +0300

    Add initial projects.

---
+++ ziel-be/src/test/java/com/greendal/ziel/ZielApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greendal.ziel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZielApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
