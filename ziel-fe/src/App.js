ad8216f9a4a04fb541682e335757dcd073c9f1c1
peppemintu <hugo.greendal@gmail.com>
2025-03-31 19:37:36 +0300

    [FE] Add routing and MUI dependencies

--- ziel-fe/src/App.js
+++ ziel-fe/src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import PropTypes from 'prop-types';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import CssBaseline from '@mui/material/CssBaseline';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
 
-function App() {
+const drawerWidth = 240;
+
+function ListItemLink(props) {
+  const { icon, primary, to } = props;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ListItemButton component={Link} to={to}>
+      {icon ? <ListItemIcon>{icon}</ListItemIcon> : null}
+      <ListItemText primary={primary} />
+    </ListItemButton>
   );
 }
 
-export default App;
+ListItemLink.propTypes = {
+  icon: PropTypes.element,
+  primary: PropTypes.string.isRequired,
+  to: PropTypes.string.isRequired,
+};
+
+export default function App() {
+  return (
+    <Router>
+      <Box sx={{ display: 'flex' }}>
+        <CssBaseline />
+        <Drawer
+          variant="permanent"
+          sx={{
+            width: drawerWidth,
+            flexShrink: 0,
+            [`& .MuiDrawer-paper`]: { width: drawerWidth, boxSizing: 'border-box' },
+          }}
+        >
+          <Toolbar />
+          <Box sx={{ overflow: 'auto' }}>
+            <List>
+              <ListItem disablePadding>
+                <ListItemLink to="/register" primary="Register" />
+              </ListItem>
+              <ListItem disablePadding>
+                <ListItemLink to="/login" primary="Login" />
+              </ListItem>
+              <ListItem disablePadding>
+                <ListItemLink to="/dashboard" primary="Dashboard" />
+              </ListItem>
+            </List>
+          </Box>
+        </Drawer>
+
+        <Routes>
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </Box>
+    </Router>
+  );
+}
+
+function Register() {
+  return <h2>Register</h2>;
+}
+
+function Login() {
+  return <h2>Login</h2>;
+}
+
+function Dashboard() {
+  return <h2>Dashboard</h2>;
+}
