c4de07c6ff57320dfafb40ca18dffb58d8a0517b
peppemintu <hugo.greendal@gmail.com>
2025-04-12 22:52:58 +0300

    [FE] Add fields and more validation to Register

--- ziel-fe/src/Register.js
+++ ziel-fe/src/Register.js
@@ -1,52 +1,94 @@
-import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Paper from '@mui/material/Paper';
 import TextField from '@mui/material/TextField';
+import {
+    FormControl,
+    InputLabel,
+    MenuItem,
+    Select,
+    Stack,
+} from '@mui/material';
 
 import { useState } from 'react';
 
 export default function Register() {
-  const [email, setEmail] = useState("");
-  const [empty, setEmpty] = useState(false);
+    const [formData, setFormData] = useState({
+      email: "",
+      password: "",
+      username: "",
+      role: "",
+    });
 
-  function handleInputChange(event) {
-      setEmail(event.target.value);
-  }
+    const [invalid, setInvalid] = useState(false);
+    const isValidEmail = (email) => /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/.test(email.trim());
 
-  function handleSubmit(event) {
-      event.preventDefault();
+    const handleChange = (fieldName) => (event) => {
+        setFormData({
+            ...formData,
+            [fieldName]: event.target.value,
+        });
+    }
 
-      if (email.trim() === "") {
-        setEmpty(true);
-      }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+
+        if (isValidEmail(formData.email)) {
+            setInvalid(false);
+            console.log(formData.email);
+            console.log(formData.password);
+            console.log(formData.username);
+            console.log(formData.role);
+        } else {
+            setInvalid(true);
+        }
     }
 
   return (
-    <Box sx={{p: 10}}>
     <form onSubmit={handleSubmit}>
-      <Paper elevation={2}>
-        <div>
+    <Paper elevation={2} sx={{ m: 10, p: 5 }}>
+      <Stack spacing={2}>
           <TextField
             sx={{margin: 2}}
             label="e-mail"
             variant="outlined"
             margin="normal"
-            helperText={empty ? "Cannot be empty." : ""}
+            helperText={invalid ? "Invalid email." : ""}
             autoFocus
-            onChange={handleInputChange}
-            error={empty}
+            onChange={handleChange("email")}
+            error={invalid}
+            required
           />
-        </div>
-        <div>
           <TextField
+            name="password"
             sx={{margin: 2}}
             label="password"
             variant="outlined"
             margin="normal"
-            name="password"
+            onChange={handleChange("password")}
+            required
           />
-        </div>
-        <div>
+          <TextField
+              sx={{margin: 2}}
+              label="username"
+              variant="outlined"
+              margin="normal"
+              name="username"
+              onChange={handleChange("username")}
+          />
+        <FormControl required sx={{ m: 2, minWidth: 200 }}>
+          <InputLabel id="role-label">Role</InputLabel>
+          <Select
+            labelId="role-label"
+            value={formData.role}
+            label="Role *"
+            onChange={handleChange("role")}
+          >
+            <MenuItem value={"STUDENT"}>Student</MenuItem>
+            <MenuItem value={"TEACHER"}>Teacher</MenuItem>
+            <MenuItem value={"ADMIN"}>Admin</MenuItem>
+          </Select>
+        </FormControl>
           <Button
             variant="contained"
             sx={{margin: 2}}
@@ -54,9 +96,8 @@ export default function Register() {
           >
             Register
           </Button>
-        </div>
+      </Stack>
       </Paper>
     </form>
-    </Box>
   );
 }
